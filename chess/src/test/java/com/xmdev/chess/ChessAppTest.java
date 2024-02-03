8213d777dd60824c963cff63d63e3b93f0d73c20
Xerckiem <mercadoxerckiem@gmail.com>
2024-02-02 21:17:25 -0500

    added test file

---
+++ chess/src/test/java/com/xmdev/chess/ChessAppTest.java
@@ -0,0 +1,20 @@
+package com.xmdev.chess;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ChessAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
